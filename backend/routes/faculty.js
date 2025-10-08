ea8e89f8675be5d69eb9a8e01dab219f37992977
harshit-ig <sharshit0369@gmail.com>
2025-10-09 01:21:59 +0530

    sort

--- backend/routes/faculty.js
+++ backend/routes/faculty.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
     }
 
     const faculty = await Faculty.find(query)
-      .sort({ name: 1 })
+      .sort({ createdAt: 1 })
       .skip(skip)
       .limit(limit);
 
